e209a152ffa60852f03e570d250cda52f162cc05
Ea-Sir <emmashpkor@gmail.com>
2026-07-01 15:16:52 +0300

    Create customers table in financial_risk_dw database

--- sql/sql/sql/02_create_tables.sql
+++ sql/sql/sql/02_create_tables.sql
@@ -1,1 +1,15 @@
+USE financial_risk_dw;
 
+CREATE TABLE customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    gender ENUM('Male','Female','Other') NOT NULL,
+    date_of_birth DATE NOT NULL,
+    occupation VARCHAR(100),
+    annual_income DECIMAL(12,2),
+    city VARCHAR(100),
+    country VARCHAR(100),
+    join_date DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
